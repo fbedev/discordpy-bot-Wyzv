6838d422522145626783add9248075a55cfb19a9
fbe_dev <97958311+fbedev@users.noreply.github.com>
2023-09-16 15:17:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
-# This example requires the 'message_content' privileged intents
-
 import os
 import discord
-from discord.ext import commands
+from pymongo import MongoClient
 
+# Get the Discord token from the environment variable
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+# Define MongoDB connection details using environment variables
+MONGO_HOST = os.getenv('MONGOHOST')
+MONGO_PORT = os.getenv('MONGOPORT')
+MONGO_USER = os.getenv('MONGOUSER')
+MONGO_PASSWORD = os.getenv('MONGOPASSWORD')
+MONGO_URL = os.getenv('MONGO_URL')
 
+# Initialize the MongoDB client
+client = MongoClient(host=MONGO_HOST, port=int(MONGO_PORT), username=MONGO_USER, password=MONGO_PASSWORD, authSource='admin')
+
+# Access your MongoDB database
+db = client['clicker']
+
+# Access a specific collection in the database
+collection = db['fbe']
+
+# Create a Discord bot
+bot = discord.Bot()
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}")
+    print(f'Logged in as {bot.user.name}')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+# Your other bot commands and event handlers can go here
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
-
-
-bot.run(os.environ["DISCORD_TOKEN"])
+# Run the bot with the Discord token
+bot.run(DISCORD_TOKEN)
